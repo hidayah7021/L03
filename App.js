0eb28d98c3a75aa2ebce2c96d2b2e96bd90df931
hidayah7021 <22007021@myrp.edu.sg>
2024-11-11 14:30:57 +0800

    should update

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { TouchableOpacity, Alert, StyleSheet, Text, TextInput, View, ToastAndroid } from 'react-native';
+import RNPickerSelect from 'react-native-picker-select';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    const [username, setUsername] = useState('');
+    const [pw, setPw] = useState('');
+    const [userType, setUserType] = useState(''); // State to store user type
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    return (
+        <View style={{ padding: 20, paddingTop: 50 }}>
+            {/*Exercise 1B*/}
+            <Text>User Type:</Text>
+            <RNPickerSelect
+                onValueChange={(value) => setUserType(value)} // Set user type
+                items={[
+                    { label: 'Admin', value: 'Admin' },
+                    { label: 'Guest', value: 'Guest' },
+                ]}
+            />
+            <Text>Username:</Text>
+            <TextInput
+                style={{ borderWidth: 1 }}
+                onChangeText={(text) => setUsername(text)}
+            />
+            <Text>Password:</Text>
+            <TextInput
+                style={{ borderWidth: 1 }}
+                onChangeText={(text) => setPw(text)}
+                secureTextEntry
+            />
+            <TouchableOpacity onPress={() => ToastAndroid.show('Welcome ' + userType + ' ' + username, ToastAndroid.SHORT)}>
+                <Text>Log in</Text>
+            </TouchableOpacity>
+            <StatusBar style="auto" />
+        </View>
+    );
+}
